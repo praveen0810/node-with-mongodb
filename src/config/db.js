42bd5788d905477ac1d8eb2d40aef0e63ce6fe13
praveen <praveenhoswami61498@gmail.com>
2023-06-24 19:19:18 +0530

    connected mongodb with node

---
+++ src/config/db.js
@@ -0,0 +1,25 @@
+import { MongoClient } from 'mongodb';
+
+let db = null;
+const dbName = process.env.MONGO_DB
+const client = new MongoClient(`mongodb://127.0.0.1:27017/${dbName}`, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+const connectDB = async () => {
+    try {
+        await client.connect();
+        console.log("Database is connected")
+    } catch (error) {
+        console.log(`Database Error======>: ${error} `)
+        process.exit(1)
+    }
+}
+function getDB() {
+    return client.db(dbName)
+}
+
+export {
+    connectDB,
+    getDB,
+}
